2f152d9a25174a0fe3dd5b8fbd5964f02b4e8cba
Javier Vela <javier.vela00@gmail.com>
2024-07-24 13:21:14 +0200

    Update prompt

--- src/seqia_gen/prompt.py
+++ src/seqia_gen/prompt.py
@@ -3,32 +3,30 @@ from langchain.prompts import PromptTemplate
 
 custom_prompt = """
 Por favor, analiza el siguiente texto y extrae la información relacionada con la sequía climatológica, sus impactos y la localización de los impactos.
-Estructura la información estrictamente en formato JSON de la siguiente manera y no incluyas ninguna otra explicación:
+Estructura la información estrictamente en formato JSON de la siguiente manera sin incluir ninguna otra explicación:
 {{
     "drought": "True/False",
-    "impacts": [] // Lista de impactos con la estructura mencionada a continuación
+    "impacts": []
 }}
-Únicamente incluye impactos si existen menciones a la sequía climatológica.
-Si bien no hay menciones a la sequía climatológica, por favor, responde con: 
+Solo incluye impactos si hay menciones a la sequía climatológica. Si no hay menciones a la sequía climatológica, responde con:
 {{
     "drought": "False",
     "impacts": []
 }}
-Extraee cada uno de los impactos en una estructura de la siguiente manera:
+Extrae cada uno de los impactos en la siguiente estructura:
 {{
-	"impact_class": "impact class",
-	"impact": "impact description",
-	"location": [{{"name": "location","type": "location_type"}}, ... ],
+    "impact_class": "impact class",
+    "impact": "impact description",
+    "location": [{{"name": "location", "type": "location_type"}}, ...]
 }}
-A los impactos extraídos asignales una clase ("impact_class") de la siguiente lista: {impacts}.
-Si bien uno de los impactos no aparece, puedes añadirlo en una clase "other" y poner entre paréntesis la nueva clase que tú le asignarías de la siguiente manera: "other (new impact class)"
-En el campo "impact" inserta una descripción corta del impacto.
-En el campo "location" inserta una lista de las localizaciones afectadas por el impacto. Por cada localización inserta su nombre "name" y tipo de localización "type". Es posible que localizaciones con el mismo nombre se refieran a tipos diferentes (provincia/ciudad). Ejemplos: Comunidad de Madrid (tipo: comunidad autonoma), Zaragoza (provincia o municipio), Ebro (rio o cuenca).
-Quiero que la respuesta sea únicamente un JSON válido y de la misma estructura que la mencionada. Por lo que no añadas ningún otro texto adicional.
+Asigna cada impacto a una clase ("impact_class") de la siguiente lista: {impacts}.
+Si uno de los impactos no aparece, añádelo en una clase "other" y pon entre paréntesis la nueva clase que tú le asignarías de la siguiente manera: "other (clase)".
+En el campo "impact" incluye una descripción muy breve del impacto.
+En el campo "location" incluye una lista de las localizaciones afectadas por el impacto. Para cada localización, incluye su nombre "name" y tipo de localización "type". Es posible que localizaciones con el mismo nombre se refieran a tipos diferentes (e.g. provincia o ciudad). Ejemplos: Comunidad de Madrid (comunidad autónoma), Zaragoza (provincia), Ebro (río), cuenca del Duero (cuenca).
+Quiero que la respuesta sea únicamente un JSON válido con la estructura mencionada. No añadas ningún otro texto adicional.
 Texto:
 {text}
 """
-
 
 # Create the prompt template
 prompt = PromptTemplate(template=custom_prompt, input_variables=["text", "impacts"])
