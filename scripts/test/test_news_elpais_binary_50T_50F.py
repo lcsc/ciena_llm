4e388ca69301685e68f4fcf41c8f5e954dcdee66
Javier Vela <javier.vela00@gmail.com>
2024-07-24 15:48:39 +0200

    Improve src structure

---
+++ scripts/test/test_news_elpais_binary_50T_50F.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+from seqia_gen import ClimateImpactExtractor
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+TEST_NAME = "news-elpais-binary-50T-50F"
+# TEST_NAME = "news-elpais-binary-50T-50F"
+DATASET_PATH = (
+    f"/home/javier/Developer/SeqIA/data/test-datasets-small/{TEST_NAME}/sample"
+)
+RESULTS_DIR = f"./results/{TEST_NAME}/"
+os.makedirs(os.path.dirname(RESULTS_DIR), exist_ok=True)
+
+
+extractor = ClimateImpactExtractor()
+
+articles = extractor(
+    dataset_path=DATASET_PATH,
+)
+
+extractor.write_excluded_problematic_articles_to_csv(file=f"{RESULTS_DIR}/excluded.csv")
+
+extractor.write_summary_to_csv(
+    articles,
+    f"{RESULTS_DIR}/summary.csv",
+)
--- src/seqia_gen/__init__.py
+++ src/seqia_gen/__init__.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from typing import Optional
+from typing import Optional, List
 from tqdm import tqdm
 
 import dotenv
@@ -14,73 +14,44 @@ from seqia.utils.output import write_summary_to_csv
 
 from seqia_gen.prompt import prompt
 from seqia_gen.response import parse_response
-
-# Setup logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+from seqia_gen.config.loader import ConfigLoader
 
 
-TEST_NAME = "news-elpais-binary-50T-50F"
-# TEST_NAME = "news-elpais-binary-50T-50F"
-DATASET_PATH = (
-    f"/home/javier/Developer/SeqIA/data/test-datasets-small/{TEST_NAME}/sample"
-)
-RESULTS_DIR = f"./results/{TEST_NAME}/"
-os.makedirs(os.path.dirname(RESULTS_DIR), exist_ok=True)
+class ClimateImpactExtractor:
+    def __init__(self, model_name: str = "llama3"):
 
+        self.config = ConfigLoader().config
+        self.article_loader = ArticleLoader()
 
-# Initialize the Ollama model
-llm = Ollama(model="llama3")
+        # Initialize the Ollama model
+        self.llm = Ollama(model=model_name)
 
-# Create the LangChain with the LLM and the prompt template
-chain = prompt | llm
+        # Create the LangChain with the LLM and the prompt template
+        self.chain = prompt | self.llm
 
-impacts = [
-    "agriculture",
-    "farming",
-    "hidrological_resources",
-    "energy",
-    "other",
-]
+        # Load impacts from configuration
+        self.impacts = self.config["impacts"]
 
+    def __call__(self, dataset_path: str) -> List[Article]:
+        articles = self.article_loader(dataset_path)
 
-def extract(article: Article) -> Optional[Article]:
-    text = article.get_headline_and_body(separator=".")
-    response = chain.invoke({"text": text, "impacts": impacts})
-    article, parsed = parse_response(article, response)
+        for article in tqdm(articles, desc="Extracting impacts from articles."):
+            
+            article = self.extract(article)
 
-    if parsed:
-        return article
+            if article:
+                logging.debug(f"Article {article.filename}:\n{article}")
 
+    def extract(self, article: Article) -> Optional[Article]:
+        text = article.get_headline_and_body(separator=".")
+        response = self.chain.invoke({"text": text, "impacts": self.impacts})
+        article, parsed = parse_response(article, response)
 
-def main():
-    loader = ArticleLoader()
-    articles = loader(DATASET_PATH)
+        if parsed:
+            return article
 
-    loader.write_excluded_problematic_articles_to_csv(f"{RESULTS_DIR}/excluded.csv")
+    def write_excluded_problematic_articles_to_csv(file: str):
+        self.article_loader.write_excluded_problematic_articles_to_csv(file)
 
-    for article in tqdm(articles, desc="Extracting impacts from articles."):
-        article = extract(article)
-
-        if article:
-            logging.debug(f"Article {article.filename}:\n{article}")
-
-    write_summary_to_csv(
-        articles,
-        f"{RESULTS_DIR}/summary.csv",
-        [
-            "article_filename",
-            "article_drought",
-            "article_impacts_aggregated",
-            "article_locations_aggregated",
-            "article_locations_aggregated",
-            "article_date",
-            "article_url",
-            "article_headline",
-        ],
-    )
-
-
-if __name__ == "__main__":
-    main()
+    def write_summary_to_csv(articles: List[Article], file: str):
+        write_summary_to_csv(articles, file, self.config["output"]["summary"])
