1a6712eb7215d6c7edb833d7f98214631c57844a
Javier Vela <javier.vela00@gmail.com>
2024-10-03 18:33:55 +0200

    Refactor impact extractor WIP

--- src/ciena_llm/extractor/impact.py
+++ src/ciena_llm/extractor/impact.py
@@ -6,48 +6,48 @@ from seqia.article import Article
 
 from ciena_llm.llm import LLM
 from ciena_llm.prompt_template_manager import PromptTemplateManager
-from ciena_llm.response.drought import DroughtLLMResponse
+from ciena_llm.response.impact import ImpactLLMResponse
 
 
-class DroughtExtractor:
+class ImpactExtractor:
     def __init__(self, config: Dict):
         """
-        Initialize the DroughtExtractor with the given configuration.
+        Initialize the ImpactExtractor with the given configuration.
 
-        :param config: The configuration for the DroughtExtractor.
+        :param config: The configuration for the ImpactExtractor.
         """
         self.ptm = PromptTemplateManager()
 
         # Create prompt templates
-        self.drought_prompt_template = self.ptm.get_prompt_template(
-            config["prompt"]["drought_extraction"]
+        self.impact_prompt_template = self.ptm.get_prompt_template(
+            config["prompt"]["impact_extraction"]
         )
-        self.drought_response_parser = JsonOutputParser(
-            pydantic_object=DroughtLLMResponse
+        self.impact_response_parser = JsonOutputParser(
+            pydantic_object=ImpactLLMResponse
         )
-        self.drought_response_parser_prompt_template = self.ptm.get_prompt_template(
-            config["prompt"]["drought_response_parser"],
-            format_instructions=self.drought_response_parser.get_format_instructions(),
+        self.impact_response_parser_prompt_template = self.ptm.get_prompt_template(
+            config["prompt"]["impact_response_parser"],
+            format_instructions=self.impact_response_parser.get_format_instructions(),
         )
 
         # Create LLMs
-        self.drought_llm = LLM(
+        self.impact_llm = LLM(
             config=config["llm"],
-            stage="drought_extraction",
+            stage="impact_extraction",
         )
-        self.drought_response_parser_llm = LLM(
+        self.impact_response_parser_llm = LLM(
             config=config["llm"],
-            stage="drought_response_parser",
+            stage="impact_response_parser",
         )
 
-    def extract_drought(self, article: Article) -> bool:
+    def extract_impact(self, article: Article) -> bool:
         text = article.get_headline_and_body(separator=".")
 
-        # Check context length for drought prompt
-        self.drought_llm.check_context_length(text, self.drought_prompt_template)
+        # Check context length for impact prompt
+        self.impact_llm.check_context_length(text, self.impact_prompt_template)
 
-        drought_chain = self.drought_prompt_template | self.drought_llm
-        drought_response = drought_chain.invoke({"text": text})
-        drought_response = parse_response_bool(drought_response)
+        impact_chain = self.impact_prompt_template | self.impact_llm
+        impact_response = impact_chain.invoke({"text": text})
+        impact_response = parse_response_bool(impact_response)
 
-        return drought_response
+        return impact_response
