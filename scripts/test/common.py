18e58017cb49e8284cef723dd15608c543bed680
Javier Vela <javier.vela00@gmail.com>
2024-10-28 13:00:18 +0100

    Refactor ClimateImpactExtractorEvaluation class to separate evaluation of drought and individual impacts

--- scripts/test/common.py
+++ scripts/test/common.py
@@ -168,8 +168,8 @@ class ClimateImpactExtractorTest:
 
 
 IMPACT_EVALUATION_TYPE = "impact"
-# LOCATION_EVALUATION_TYPE = "location"
-EVALUATION_TYPES = [IMPACT_EVALUATION_TYPE]  # , LOCATION_EVALUATION_TYPE]
+DROUGHT_EVALUATION_TYPE = "drought"
+EVALUATION_TYPES = [IMPACT_EVALUATION_TYPE, DROUGHT_EVALUATION_TYPE]
 
 
 class ClimateImpactExtractorEvaluation:
@@ -194,6 +194,8 @@ class ClimateImpactExtractorEvaluation:
     def run(self):
         if self.evaluation_type == IMPACT_EVALUATION_TYPE:
             self.evaluate_impact()
+        elif self.evaluation_type == DROUGHT_EVALUATION_TYPE:
+            self.evaluate_drought()
         else:
             raise Exception(
                 f"Invalid evaluation type {self.evaluation_type}. Must be one of {EVALUATION_TYPES}"
@@ -212,7 +214,7 @@ class ClimateImpactExtractorEvaluation:
             lambda x: x.split("/")[-1]
         )
 
-        # Get a evaluation dataframe with the entries from the anntations (the truth) and the results articles (the predictions). Link the true and predictions by the URL.
+        # Get a evaluation dataframe with the entries from the annotations (the truth) and the results articles (the predictions). Link the true and predictions by the URL.
         df_evaluation = pd.merge(
             df_annotations,
             df_results,
@@ -221,15 +223,35 @@ class ClimateImpactExtractorEvaluation:
             how="inner",
         )
 
-        # From the evaluation df, get drought and each impact's true and predicted labels
+        # Evaluate each impact
+        for impact in self.impact_names:
+            self.evaluate_single_impact(df_evaluation, impact)
+
+    def evaluate_drought(self):
+        # Load the impact results as pandas dataframe
+        impact_results_path = f"{self.results_dir}/summary.csv"
+        df_results = pd.read_csv(impact_results_path)
+        # Load the annotations
+        df_annotations = pd.read_csv(self.annotations_path)
+        # Evaluate the impact results
+
+        df_results["article_filepath"] = df_results["article_filename"]
+        df_results["article_filename"] = df_results["article_filepath"].apply(
+            lambda x: x.split("/")[-1]
+        )
+
+        # Get a evaluation dataframe with the entries from the annotations (the truth) and the results articles (the predictions). Link the true and predictions by the URL.
+        df_evaluation = pd.merge(
+            df_annotations,
+            df_results,
+            left_on="url",
+            right_on="article_url",
+            how="inner",
+        )
+
+        # From the evaluation df, get drought's true and predicted labels
         df_evaluation["drought_true"] = df_evaluation["drought_label"]
         df_evaluation["drought_predicted"] = df_evaluation["article_drought"]
-
-        for impact in self.impact_names:
-            df_evaluation[f"{impact}_true"] = df_evaluation[f"{impact}_label"]
-            df_evaluation[f"{impact}_predicted"] = df_evaluation[
-                "article_impacts_aggregated"
-            ].apply(lambda x: impact in x)
 
         # Plot confusion matrix and calculate metrics for drought label
         cm, metrics = compute_confusion_matrix_and_metrics(df_evaluation, "drought")
@@ -238,10 +260,16 @@ class ClimateImpactExtractorEvaluation:
         plot_confusion_matrix("drought", cm, metrics, self.plot_dir)
         print("-" * 50)
 
+    def evaluate_single_impact(self, df_evaluation, impact):
+        # From the evaluation df, get each impact's true and predicted labels
+        df_evaluation[f"{impact}_true"] = df_evaluation[f"{impact}_label"]
+        df_evaluation[f"{impact}_predicted"] = df_evaluation[
+            "article_impacts_aggregated"
+        ].apply(lambda x: impact in x)
+
         # Plot confusion matrix and calculate metrics for each impact
-        for impact in self.impact_names:
-            cm, metrics = compute_confusion_matrix_and_metrics(df_evaluation, impact)
-            print_confusion_matrix(impact, cm)
-            print_metrics(impact, **metrics)
-            plot_confusion_matrix(impact, cm, metrics, self.plot_dir)
-            print("-" * 50)
+        cm, metrics = compute_confusion_matrix_and_metrics(df_evaluation, impact)
+        print_confusion_matrix(impact, cm)
+        print_metrics(impact, **metrics)
+        plot_confusion_matrix(impact, cm, metrics, self.plot_dir)
+        print("-" * 50)
