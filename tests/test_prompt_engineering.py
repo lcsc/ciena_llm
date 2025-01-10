8153bb435487e47c1bfb644718f9dce186a88b23
Javier Vela <javier.vela00@gmail.com>
2025-01-10 12:54:40 +0100

    Refactor test structure for prompt engineering to support multiple test types

--- tests/test_prompt_engineering.py
+++ tests/test_prompt_engineering.py
@@ -4,29 +4,39 @@ from common import ClimateImpactExtractorTest, ClimateImpactExtractorEvaluation
 DATASET_DIR = "news-elpais-sample-194-annotated-e2e"
 DATASET_BASE_PATH = f"/home/javier/Developer/SeqIA/data/{DATASET_DIR}/"
 
-SHORT_TEST = False
-# TODO
-if not SHORT_TEST:
+BASE_TEST_NAME = "prompt-engineering"
+
+TEST_TYPE = "impacts"  # "e2e" or "impacts" or "short"
+
+
+if TEST_TYPE == "e2e":
     DATASET_DIR = "news-elpais-sample-194-annotated-e2e"
     DATASET_BASE_PATH = f"/home/javier/Developer/SeqIA/data/{DATASET_DIR}/"
     DATASET_PATH = f"{DATASET_BASE_PATH}/sample/"
     # DATASET_PATH = f"{DATASET_BASE_PATH}/sample-pred-drought/"
     ANNOTATION_PATH = f"{DATASET_BASE_PATH}/dataset.csv"
-else:
+    BASE_TEST_NAME = f"{BASE_TEST_NAME}/e2e"
+elif TEST_TYPE == "impacts":
+    DATASET_BASE_PATH = (
+        "/home/javier/Developer/SeqIA/data/news-elpais-grupoz-annotated-impacts/"
+    )
+    DATASET_PATH = f"{DATASET_BASE_PATH}/sample"
+    ANNOTATION_PATH = f"{DATASET_BASE_PATH}/all.csv"
+    BASE_TEST_NAME = f"{BASE_TEST_NAME}/impacts"
+elif TEST_TYPE == "short":
     DATASET_BASE_PATH = (
         "/home/javier/Developer/SeqIA/data/test-datasets-small/test-jvela-1/"
     )
     DATASET_PATH = f"{DATASET_BASE_PATH}/sample"
     # DATASET_PATH = "/home/javier/Developer/SeqIA/data/test-datasets-small/news-elpais-binary-5T-2F/sample"
     ANNOTATION_PATH = f"{DATASET_BASE_PATH}/labels.csv"
-
-BASE_TEST_NAME = f"news-elpais-e2e-impacts-prompt-engineering"
+    BASE_TEST_NAME = f"{BASE_TEST_NAME}/short"
+else:
+    raise ValueError(f"Unknown TEST_TYPE: {TEST_TYPE}")
 
 
 def run_test(test_name_suffix, summarization_enable, response_parsing_enable, language):
     TEST_NAME = f"{BASE_TEST_NAME}/{test_name_suffix}-{language}"
-    if SHORT_TEST:
-        TEST_NAME = f"{TEST_NAME}-short"
     OVERRIDE_CONFIG = {
         "pipeline": {
             "summarization": {
@@ -46,10 +56,11 @@ def run_test(test_name_suffix, summarization_enable, response_parsing_enable, la
     test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH, OVERRIDE_CONFIG)
     test.run()
 
-    eval_drought = ClimateImpactExtractorEvaluation(
-        TEST_NAME, ANNOTATION_PATH, "drought"
-    )
-    eval_drought.run()
+    # TODO parametrize based on test hacing drought label or not. Or just check in text and return None if not found
+    # eval_drought = ClimateImpactExtractorEvaluation(
+    #     TEST_NAME, ANNOTATION_PATH, "drought"
+    # )
+    # eval_drought.run()
 
     eval_impact = ClimateImpactExtractorEvaluation(TEST_NAME, ANNOTATION_PATH, "impact")
     eval_impact.run()
