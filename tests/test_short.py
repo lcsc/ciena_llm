66885a8d97f46da3bbfe19a5ed01547c50696f67
Javier Vela <javier.vela00@gmail.com>
2025-03-12 09:24:56 +0100

    Move old tests to 0.2.0 folder and create new test for 0.3.0

--- tests/test_short.py
+++ tests/test_short.py
@@ -9,55 +9,66 @@ DATASET_PATH = f"{DATASET_BASE_PATH}/sample"
 ANNOTATION_PATH = f"{DATASET_BASE_PATH}/labels.csv"
 
 LANGUAGE = "es"
-MODEL = "gemma2:9b"
-
+MODEL = "llama3.2:3b"
 
 OVERRIDE_CONFIG = {
-    # "task": "impact",
-    "pipeline": {
-        "extraction": {
-            "enable": True,
-            "prompt": {
-                "language": LANGUAGE,
-            },
-        },
+    "llm": {"name": MODEL},
+    "stages": {
         "summarization": {
             "enable": True,
-            "prompt": {
-                "language": LANGUAGE,
+            "steps": {
+                "summarization": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
             },
         },
-        "response_parsing": {
+        "event_identification": {
             "enable": False,
-            "prompt": {
-                "language": LANGUAGE,
+        },
+        "impact_extraction": {
+            "enable": True,
+            "steps": {
+                "extraction": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "category": "multi_classification",
+                        "subcategory": "description",
+                    },
+                },
+                "response_parsing": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
             },
         },
-    },
-    "llm": {
-        "default": {"name": MODEL},
+        "location_extraction": {
+            "enable": True,
+            "steps": {
+                "extraction": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "category": "extraction",
+                    },
+                },
+                "response_parsing": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
+            },
+        },
     },
 }
 
-impact_override_config = OVERRIDE_CONFIG.copy()
-impact_override_config["task"] = "impact"
-impact_override_config["pipeline"]["extraction"]["prompt"][
-    "category"
-] = "multi_classification"
-impact_override_config["pipeline"]["extraction"]["prompt"][
-    "subcategory"
-] = "description"
-TEST_NAME = f"{BASE_TEST_NAME}/impact"
+TEST_NAME = f"{BASE_TEST_NAME}"
 
-test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH, impact_override_config)
-test.run()
-
-
-province_override_config = OVERRIDE_CONFIG.copy()
-province_override_config["task"] = "province"
-province_override_config["pipeline"]["extraction"]["prompt"]["category"] = "extraction"
-province_override_config["pipeline"]["extraction"]["prompt"]["subcategory"] = None
-TEST_NAME = f"{BASE_TEST_NAME}/province"
-
-test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH, province_override_config)
+test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH, OVERRIDE_CONFIG)
 test.run()
