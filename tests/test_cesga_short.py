a0cb416a959c41e6f6ccaf16bb6ca7bf55b35181
Javier Vela <javier.vela00@gmail.com>
2025-03-17 11:02:31 +0100

    Add new test for CESGA

---
+++ tests/test_cesga_short.py
@@ -0,0 +1,75 @@
+from common import ClimateImpactExtractorTest
+
+
+BASE_TEST_NAME = "test_short"
+DATASET_BASE_PATH = (
+    "/home/csic/hia/jvt/CienaLLM/data/test-datasets-small/news-elpais-binary-2T-1F/"
+)
+DATASET_PATH = f"{DATASET_BASE_PATH}/sample"
+
+LANGUAGE = "es"
+MODEL = "llama3.2:3b"
+
+OVERRIDE_CONFIG = {
+    "llm": {"name": MODEL},
+    "stages": {
+        "summarization": {
+            "enable": True,
+            "steps": {
+                "summarization": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
+            },
+        },
+        "event_identification": {
+            "enable": False,
+        },
+        "impact_extraction": {
+            "enable": True,
+            "steps": {
+                "extraction": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "step": "extraction",
+                        "category": "description",
+                    },
+                },
+                "response_parsing": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
+            },
+        },
+        "location_extraction": {
+            "enable": True,
+            "steps": {
+                "extraction": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "step": "extraction",
+                        "category": "province",
+                    },
+                },
+                "response_parsing": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "category": "province",
+                    },
+                },
+            },
+        },
+    },
+}
+
+TEST_NAME = f"{BASE_TEST_NAME}"
+
+test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH, OVERRIDE_CONFIG)
+test.run()
