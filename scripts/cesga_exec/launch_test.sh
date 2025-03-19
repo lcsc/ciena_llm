0be512348ced8f66abe22266ce718abe7ac97dba
Javier Vela <javier.vela00@gmail.com>
2025-03-19 13:21:29 +0100

    Update results directory path format in launch_test.sh for consistency

--- scripts/cesga_exec/launch_test.sh
+++ scripts/cesga_exec/launch_test.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 export CIENA_LLM_DIR="$HOME/CienaLLM"
 export TEST_NAME="test_cesga_short_env_delete" # REMOVE Rename test
-export RESULTS_DIR="$CIENA_LLM_DIR/results/$TEST_NAME/$(date +%Y%m%d_%H%M%S)/"
+export RESULTS_DIR="$CIENA_LLM_DIR/ciena_llm/results/$TEST_NAME/$(date +%Y-%m-%d_%H-%M-%S)/"
 export DATASET_PATH="$CIENA_LLM_DIR/data/test-datasets-small/news-elpais-binary-2T-1F/sample"
 export CIENA_LLM_MODEL="llama3.2:1b"
 export CIENA_LLM_LANGUAGE="en"
