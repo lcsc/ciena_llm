17220c73730d80a1e7d9eb28c3f8fb9ffd3bc96f
Javier Vela <javier.vela00@gmail.com>
2025-03-25 11:41:57 +0100

    Add CESGA results synchronization script

---
+++ scripts/cesga_exec/sync_cesga_results.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+REMOTE_PATH="CienaLLM/ciena_llm/results/"
+LOCAL_PATH="/home/javier/Developer/ciena_llm/results_cesga/"
+
+rsync -a --info=progress2 --info=name0 -e ssh cesga:$REMOTE_PATH $LOCAL_PATH
