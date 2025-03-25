3c08111f42567e3d2db9f2d3fa12c0ede3587eac
Javier Vela <javier.vela00@gmail.com>
2025-03-25 12:41:37 +0100

    Add test for climate impact extraction and evaluation with 12 articles

---
+++ tests/test_short_12.py
@@ -0,0 +1,77 @@
+from common import ClimateImpactExtractorTest, ClimateImpactExtractorEvaluation
+
+
+TEST_NAME = "test_short_12"
+DATASET_BASE_PATH = (
+    "/home/javier/Developer/SeqIA/data/test-datasets-small/cesga-test-12/"
+)
+DATASET_PATH = f"{DATASET_BASE_PATH}/sample"
+ANNOTATION_PATH = f"{DATASET_BASE_PATH}/dataset.csv"
+
+LANGUAGE = "en"
+MODEL = "llama3.2:3b"
+
+OVERRIDE_CONFIG = {
+    "llm": {"name": MODEL},
+    "stages": {
+        "summarization": {
+            "enable": False,
+            "steps": {
+                "summarization": {
+                    "enable": False,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
+            },
+        },
+        "event_identification": {
+            "enable": False,
+        },
+        "impact_extraction": {
+            "enable": True,
+            "steps": {
+                "extraction": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "step": "extraction",
+                        "category": "description",
+                    },
+                },
+                "response_parsing": {
+                    "enable": False,
+                    "prompt": {
+                        "language": LANGUAGE,
+                    },
+                },
+            },
+        },
+        "location_extraction": {
+            "enable": True,
+            "steps": {
+                "extraction": {
+                    "enable": True,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "step": "extraction",
+                        "category": "province",
+                    },
+                },
+                "response_parsing": {
+                    "enable": False,
+                    "prompt": {
+                        "language": LANGUAGE,
+                        "category": "province",
+                    },
+                },
+            },
+        },
+    },
+}
+
+test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH, OVERRIDE_CONFIG)
+test.run()
+
+eval = ClimateImpactExtractorEvaluation(TEST_NAME, ANNOTATION_PATH, "impact")
+eval.run()
