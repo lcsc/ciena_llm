7537c5c2b3cdbf8c7f10820d88edfee6d2e243fa
Javier Vela <javier.vela00@gmail.com>
2025-03-25 18:24:48 +0100

    Update CESGA test launch script to include additional models and adjust SLURM job configuration

--- scripts/cesga_exec/launch_test_cesga_12_all_models.sh
+++ scripts/cesga_exec/launch_test_cesga_12_all_models.sh
@@ -9,9 +9,7 @@ export RESULTS_DIR="$HOME/CienaLLM/ciena_llm/results/$TEST_NAME/$(date +%Y-%m-%d
 export DATASET_PATH="$HOME/CienaLLM/data/test-datasets-small/cesga-test-12/sample/"
 export ANNOTATION_PATH="$HOME/CienaLLM/data/test-datasets-small/cesga-test-12/dataset.csv"
 
-export MODELS="gemma2:2b-instruct-q4_K_M gemma2:9b-instruct-q4_K_M gemma2:9b-instruct-fp16 gemma2:27b-instruct-q4_K_M"
-# TODO: Add more models
-# export MODELS="llama3.2:3b-instruct-q4_K_M llama3.1:8b-instruct-q4_K_M llama3.1:8b-instruct-fp16 llama3.3:70b-instruct-q4_K_M gemma2:2b-instruct-q4_K_M gemma2:9b-instruct-q4_K_M gemma2:9b-instruct-fp16 gemma2:27b-instruct-q4_K_M qwen2.5:3b-instruct-q4_K_M qwen2.5:7b-instruct-q4_K_M qwen2.5:7b-instruct-fp16 qwen2.5:72b-instruct-q4_K_M"
+export MODELS="llama3.2:3b-instruct-q4_K_M llama3.1:8b-instruct-q4_K_M llama3.1:8b-instruct-fp16 llama3.3:70b-instruct-q4_K_M gemma2:2b-instruct-q4_K_M gemma2:9b-instruct-q4_K_M gemma2:9b-instruct-fp16 gemma2:27b-instruct-q4_K_M qwen2.5:3b-instruct-q4_K_M qwen2.5:7b-instruct-q4_K_M qwen2.5:7b-instruct-fp16 qwen2.5:72b-instruct-q4_K_M"
 
 export CIENA_LLM_LANGUAGE="en"
 export CIENA_LLM_SUMMARIZATION_ENABLE="False"
@@ -19,9 +17,7 @@ export CIENA_LLM_IMPACT_EXTRACTION_ENABLE="True"
 export CIENA_LLM_LOCATION_EXTRACTION_ENABLE="True"
 export CIENA_LLM_RESPONSE_PARSING_ENABLE="False"
 
-export SLURM_JOB_TIME="00:30:00"
-# TODO: Change to 3 hours
-# export SLURM_JOB_TIME="03:00:00"
+export SLURM_JOB_TIME="03:00:00"
 
 mkdir -p $RESULTS_DIR
 
@@ -31,14 +27,6 @@ sbatch \
     -t $SLURM_JOB_TIME \
     -o $RESULTS_DIR/slurm.out \
     -e $RESULTS_DIR/slurm.err \
-    -c 32 \
-    --gres=gpu:a100:1 \
+    -c 64 \
+    --gres=gpu:a100:2 \
     $DIR/ciena_sbatch_many_models.sh
-# TODO: Add more GPUs
-# sbatch \
-#     -t $SLURM_JOB_TIME \
-#     -o $RESULTS_DIR/slurm.out \
-#     -e $RESULTS_DIR/slurm.err \
-#     -c 64 \
-#     --gres=gpu:a100:2 \
-#     $DIR/ciena_sbatch_many_models.sh
