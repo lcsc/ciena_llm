9c79b64cbc232244d221f769733fcdc2fa3e3774
Javier Vela <javier.vela00@gmail.com>
2025-04-12 10:22:25 +0200

    Update model exports in launch_test_cesga_final_experiment script

--- scripts/cesga_exec/launch_test_cesga_final_experiment.sh
+++ scripts/cesga_exec/launch_test_cesga_final_experiment.sh
@@ -15,15 +15,16 @@ categories=("simple" "description")
 export DATASETS="news-elpais-grupoz-impact-complete-subset news-elpais-sample-194-annotated-e2e"
 
 # DONE:
-export MODELS="llama3.1:8b-instruct-fp16"
+# export MODELS="llama3.1:8b-instruct-fp16"
+
+export MODELS="llama3.1:8b-instruct-q4_K_M"
 
 # TODO:
-# export MODELS="llama3.1:8b-instruct-q4_K_M"
 # export MODELS="llama3.2:3b-instruct-q4_K_M"
 # export MODELS="qwen2.5:3b-instruct-q4_K_M"
 # export MODELS="qwen2.5:7b-instruct-q4_K_M"
-# export MODELS="llama3.3:70b-instruct-q4_K_M"
 # export MODELS="qwen2.5:7b-instruct-fp16"
+# export MODELS="llama3.3:70b-instruct-q4_K_M"
 # export MODELS="qwen2.5:72b-instruct-q4_K_M"
 # TODO Gemma3?
 # export MODELS="gemma2:2b-instruct-q4_K_M"
