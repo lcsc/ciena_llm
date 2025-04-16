ffd050997c0f84ea500a0037a4b7a7311a92d765
Javier Vela <javier.vela00@gmail.com>
2025-04-16 09:12:08 +0200

    Refactor launch_test_cesga_final_experiment script: update SLURM settings, structured output mode, and dataset paths

--- scripts/cesga_exec/launch_test_cesga_final_experiment.sh
+++ scripts/cesga_exec/launch_test_cesga_final_experiment.sh
@@ -12,7 +12,6 @@ export CIENA_LLM_LOCATION_EXTRACTION_ENABLE="False"
 bools=("True" "False")
 categories=("simple" "description")
 
-
 # DONE:
 # export MODELS="llama3.1:8b-instruct-fp16"
 # export MODELS="llama3.1:8b-instruct-q4_K_M"
@@ -23,20 +22,21 @@ categories=("simple" "description")
 # export MODELS="gemma3:4b-it-q4_K_M"
 # export MODELS="gemma3:12b-it-q4_K_M"
 # export MODELS="gemma3:12b-it-fp16"
+# export MODELS="llama3.3:70b-instruct-q4_K_M"
 
-export SLURM_JOB_TIME="10:00:00"
-export SLURM_CPUS=64
-export SLURM_GPUS=2
-export CIENA_LLM_STRUCTURED_OUTPUT_MODE="tool"
+export SLURM_JOB_TIME="2:00:00"
+export SLURM_CPUS=32
+export SLURM_GPUS=1
+export CIENA_LLM_STRUCTURED_OUTPUT_MODE="prompt"
 
 # export DATASETS="news-elpais-sample-194-annotated-e2e"
-export DATASETS="news-elpais-grupoz-impact-complete-subset"
+# export DATASETS="news-elpais-grupoz-impact-complete-subset"
+export DATASETS="news-elpais-sample-194-annotated-e2e news-elpais-grupoz-impact-complete-subset"
 
-export MODELS="llama3.3:70b-instruct-q4_K_M"
+export MODELS="gemma3:27b-it-q4_K_M"
 
 # TODO:
 # export MODELS="qwen2.5:72b-instruct-q4_K_M"
-# export MODELS="gemma3:27b-it-q4_K_M"
 
 for dataset in $DATASETS; do
     for model in $MODELS; do
