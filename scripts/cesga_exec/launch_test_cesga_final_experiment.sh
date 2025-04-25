2543fdb67ae80061c2bad4c2602560df651ce8f5
Javier Vela <javier.vela00@gmail.com>
2025-04-25 19:05:04 +0200

    Refactor model and category configurations in launch_test_cesga_final_experiment script

--- scripts/cesga_exec/launch_test_cesga_final_experiment.sh
+++ scripts/cesga_exec/launch_test_cesga_final_experiment.sh
@@ -12,16 +12,9 @@ export CIENA_LLM_LOCATION_EXTRACTION_ENABLE="False"
 export CIENA_LLM_STRUCTURED_OUTPUT_MODE="prompt"
 
 bools=("True" "False")
-categories=("simple" "description")
-
-# DONE:
-# export MODELS="gemma3:4b-it-q4_K_M"
-# export MODELS="gemma3:12b-it-q4_K_M"
-# export MODELS="gemma3:12b-it-fp16"
-# export MODELS="gemma3:27b-it-q4_K_M"
-# export MODELS="llama3.1:8b-instruct-fp16"
-# export MODELS="llama3.1:8b-instruct-q4_K_M"
-# export MODELS="llama3.2:3b-instruct-q4_K_M"
+# TODO change
+# categories=("simple" "description")
+categories=("simple")
 
 export SLURM_JOB_TIME="2:00:00"
 export SLURM_CPUS=32
@@ -34,11 +27,20 @@ export SLURM_GPUS=1
 # export DATASETS="news-elpais-grupoz-impact-complete-subset"
 export DATASETS="news-elpais-sample-194-annotated-e2e news-elpais-grupoz-impact-complete-subset"
 
-export MODELS="qwen2.5:3b-instruct-q4_K_M"
-# export MODELS="llama3.3:70b-instruct-q4_K_M"
-# export MODELS="qwen2.5:7b-instruct-q4_K_M"
-# export MODELS="qwen2.5:7b-instruct-fp16"
-# export MODELS="qwen2.5:72b-instruct-q4_K_M"
+# DONE:
+
+export MODELS="gemma3:4b-it-q4_K_M gemma3:12b-it-q4_K_M gemma3:12b-it-fp16"
+
+# export MODELS="gemma3:27b-it-q4_K_M" # REPEAT: SIMPLE
+# export MODELS="llama3.1:8b-instruct-fp16" # REPEAT: SIMPLE
+# export MODELS="llama3.1:8b-instruct-q4_K_M" # REPEAT: SIMPLE
+# export MODELS="llama3.2:3b-instruct-q4_K_M" # REPEAT: SIMPLE
+
+# export MODELS="qwen2.5:3b-instruct-q4_K_M" # REPEAT: ALL
+# export MODELS="qwen2.5:7b-instruct-q4_K_M" # REPEAT: ALL
+# export MODELS="qwen2.5:7b-instruct-fp16" # REPEAT: ALL
+# export MODELS="llama3.3:70b-instruct-q4_K_M" # REPEAT: ALL
+# export MODELS="qwen2.5:72b-instruct-q4_K_M" # REPEAT: ALL
 
 for dataset in $DATASETS; do
     for model in $MODELS; do
