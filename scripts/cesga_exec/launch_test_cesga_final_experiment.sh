d01cb1568ffbb2a7ff7545a9111d4fb814e984d8
Javier Vela <javier.vela00@gmail.com>
2025-04-28 15:57:49 +0200

    Update model export in launch_test_cesga_final_experiment script and add additional rsync command in sync_cesga_results script

--- scripts/cesga_exec/launch_test_cesga_final_experiment.sh
+++ scripts/cesga_exec/launch_test_cesga_final_experiment.sh
@@ -30,11 +30,11 @@ export DATASETS="news-elpais-sample-194-annotated-e2e news-elpais-grupoz-impact-
 # export MODELS="gemma3:27b-it-q4_K_M llama3.1:8b-instruct-fp16 llama3.1:8b-instruct-q4_K_M"
 # export MODELS="llama3.2:3b-instruct-q4_K_M"
 # export MODELS="qwen2.5:3b-instruct-q4_K_M"
+# export MODELS="qwen2.5:7b-instruct-fp16"
+
+export MODELS="qwen2.5:7b-instruct-q4_K_M"
+
 # export MODELS="llama3.3:70b-instruct-q4_K_M"
-# export MODELS="qwen2.5:7b-instruct-q4_K_M"
-
-export MODELS="qwen2.5:7b-instruct-fp16"
-
 # export MODELS="qwen2.5:72b-instruct-q4_K_M"
 
 for dataset in $DATASETS; do
--- scripts/cesga_exec/sync_cesga_results.sh
+++ scripts/cesga_exec/sync_cesga_results.sh
@@ -4,3 +4,4 @@ REMOTE_PATH="CienaLLM/ciena_llm/results/"
 LOCAL_PATH="/home/javier/Developer/ciena_llm/results_cesga/"
 
 rsync -a --info=progress2 --info=name0 -e ssh cesga:$REMOTE_PATH $LOCAL_PATH
+rsync -a --info=progress2 --info=name0 -e ssh cesga_alex:$REMOTE_PATH $LOCAL_PATH
