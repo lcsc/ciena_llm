4de5af7133568879d8bd16429243dea191eb9fc7
Javier Vela <javier.vela00@gmail.com>
2025-04-29 14:58:21 +0200

    Update model export configuration in launch_test_cesga_final_experiment_missing.sh

--- scripts/cesga_exec/launch_test_cesga_final_experiment_missing.sh
+++ scripts/cesga_exec/launch_test_cesga_final_experiment_missing.sh
@@ -14,8 +14,8 @@ bools=("True" "False")
 categories=("simple" "description")
 
 # export MODELS="llama3.1:8b-instruct-fp16 llama3.1:8b-instruct-q4_K_M llama3.2:3b-instruct-q4_K_M llama3.3:70b-instruct-q4_K_M qwen2.5:3b-instruct-q4_K_M qwen2.5:7b-instruct-q4_K_M qwen2.5:7b-instruct-fp16 qwen2.5:72b-instruct-q4_K_M gemma3:4b-it-q4_K_M gemma3:12b-it-q4_K_M gemma3:12b-it-fp16 gemma3:27b-it-q4_K_M"
-# export MODELS="llama3.1:8b-instruct-fp16 llama3.1:8b-instruct-q4_K_M llama3.2:3b-instruct-q4_K_M qwen2.5:3b-instruct-q4_K_M qwen2.5:7b-instruct-q4_K_M qwen2.5:7b-instruct-fp16"
-export MODELS="gemma3:4b-it-q4_K_M gemma3:12b-it-q4_K_M gemma3:12b-it-fp16 gemma3:27b-it-q4_K_M"
+export MODELS="llama3.1:8b-instruct-fp16 llama3.1:8b-instruct-q4_K_M llama3.2:3b-instruct-q4_K_M qwen2.5:3b-instruct-q4_K_M qwen2.5:7b-instruct-q4_K_M qwen2.5:7b-instruct-fp16"
+# export MODELS="gemma3:4b-it-q4_K_M gemma3:12b-it-q4_K_M gemma3:12b-it-fp16 gemma3:27b-it-q4_K_M"
 
 export SLURM_JOB_TIME="2:00:00"
 export SLURM_CPUS=32
