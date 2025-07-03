14e0c58d925e3aea7ffecd6e70518419ce6914f2
Javier Vela <javier.vela00@gmail.com>
2025-07-03 11:15:55 +0200

    Update Ollama module version to 0.9.4 in batch scripts

--- scripts/cesga_exec/ciena_sbatch.sh
+++ scripts/cesga_exec/ciena_sbatch.sh
@@ -14,7 +14,7 @@ echo $SLURM_JOBID >$RESULTS_DIR/slurm.id
 SCRIPT_NAME=${SCRIPT_NAME:-"tests/test_cesga.py"}
 
 # Load Ollama and Python modules
-module load cesga/2022 ollama/0.6.4 python/3.10.8
+module load cesga/2022 ollama/0.9.4 python/3.10.8
 
 # Maximum number of retries (for port and server check)
 MAX_RETRIES=5
--- scripts/cesga_exec/ciena_sbatch_many_models.sh
+++ scripts/cesga_exec/ciena_sbatch_many_models.sh
@@ -12,7 +12,7 @@ cd $HOME/CienaLLM/ciena_llm
 echo $SLURM_JOBID >$RESULTS_DIR/slurm.id
 
 # Load Ollama and Python modules
-module load cesga/2022 ollama/0.6.4 python/3.10.8
+module load cesga/2022 ollama/0.9.4 python/3.10.8
 
 # Maximum number of retries (for port and server check)
 MAX_RETRIES=5
