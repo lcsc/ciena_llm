9556f4d088ee1ea924b45120975e69f0b42047c9
Javier Vela <javier.vela00@gmail.com>
2025-09-16 12:32:12 -0400

    Stop tracking old scripts and add experiments folder with scripts

--- tests/v0.1_tests/test_long_articles_5.py
+++
@@ -1,8 +0,0 @@
-from common import ClimateImpactExtractorTest
-
-TEST_NAME = "test-jvela-long-articles-5"
-DATASET_DIR = "test-datasets-small"
-DATASET_PATH = f"/home/javier/Developer/SeqIA/data/{DATASET_DIR}/{TEST_NAME}/sample"
-
-test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH)
-test.run()
--- tests/v0.1_tests/test_news_gpt.py
+++
@@ -1,8 +0,0 @@
-from common import ClimateImpactExtractorTest
-
-TEST_NAME = "news-gpt-drought-news-article-generator"
-DATASET_DIR = TEST_NAME
-DATASET_PATH = f"/home/javier/Developer/SeqIA/data/{DATASET_DIR}/sample"
-
-test = ClimateImpactExtractorTest(TEST_NAME, DATASET_PATH)
-test.run()
